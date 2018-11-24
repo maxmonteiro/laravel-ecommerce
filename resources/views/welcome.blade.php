efaa60235aea8505ed9a7daddc25de86c706ac19
Maximo <maxmonteiro86@gmail.com>
2018-11-24 17:05:09 -0300

    rodapé (footer)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -138,5 +138,18 @@
             </div> <!-- end container -->
         </div> <!-- end blog-section -->
 
+        <footer>
+            <div class="footer-content container">
+                <div class="made-with">Made with <i class="fa fa-heart"></i> by Maximo Monteiro</div>
+                <ul>
+                    <li>Follow Me:</li>
+                    <li><a href="#"><i class="fa fa-globe"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div> <!-- end footer-content -->
+        </footer>
+
     </body>
 </html>
