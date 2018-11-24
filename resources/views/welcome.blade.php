a0ee68e954d8cc108f79e3efd4a1e84bc630904d
Maximo <maxmonteiro86@gmail.com>
2018-11-24 16:32:07 -0300

    'Cabeçalho'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel E-commerce</title>
+        <title>Laravel Ecommerce</title>
 
-        
+
     </head>
     <body>
         <header>
             <div class="top-nav container">
-                <div class="logo">Laravel e-commerce</div>
+                <div class="logo">Laravel Ecommerce</div>
                 <ul>
                     <li><a href="#">Shop</a></li>
                     <li><a href="#">About</a></li>
@@ -20,6 +20,23 @@
                     <li><a href="#">Cart</a></li>
                 </ul>
             </div> <!-- end top-nav -->
+
+            <div class="hero container">
+                <div class="hero-copy">
+                    <h1>Laravel Ecommerce Example</h1>
+                    <p>A pratical example of using CSS Grid for a typical website layout.</p>
+                    <div class="hero-buttons">
+                        <a href="#" class="button button-white">Button 1</a>
+                        <a href="#" class="button button-white">Button 2</a>
+                    </div>
+                </div> <!-- end hero-copy -->
+
+                <div class="hero-image">
+                    <img src="img/macbook-pro-laravel.png" alt="">
+                </div>
+            </div> <!-- end hero -->
+
+
         </header>
     </body>
 </html>
