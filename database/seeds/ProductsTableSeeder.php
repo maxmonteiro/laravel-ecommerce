63fcf30251b456fcccdeb82b548f2cbaf7def3c1
Maximo <maxmonteiro86@gmail.com>
2018-12-12 12:00:14 -0300

    Products - model, migration, seed

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,125 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name' => 'Laptop 1',
+            'slug' => 'laptop-1',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+        
+        Product::create([
+            'name' => 'Laptop 2',
+            'slug' => 'laptop-2',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 3',
+            'slug' => 'laptop-3',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 4',
+            'slug' => 'laptop-4',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 5',
+            'slug' => 'laptop-5',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 6',
+            'slug' => 'laptop-6',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 7',
+            'slug' => 'laptop-7',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 8',
+            'slug' => 'laptop-8',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 9',
+            'slug' => 'laptop-9',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+
+        Product::create([
+            'name' => 'Laptop 10',
+            'slug' => 'laptop-10',
+            'details' => '15", 1TB SSD, 16GB DDR4 RAM',
+            'price' => 1499.99,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim purus vitae 
+                scelerisque viverra. Maecenas vitae maximus lectus. Nullam vitae tincidunt eros. Praesent hendrerit mollis 
+                dui eget dignissim. ',
+            //'category_id' => 1
+        ]);
+    }
+}
